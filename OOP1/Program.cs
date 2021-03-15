7b73c8bff2f717d4f9bbd25e194310eab167a5bf
BurakBozer <burak.bozer11@gmail.com>
2021-03-15 09:07:43 +0300

    Projects

---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.Id = 1;
+            product1.CategoryId = 11;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 25;
+            product1.UnitsInStock = 70;
+
+            Product product2 = new Product { Id = 2 , CategoryId = 22 , ProductName = "Sandalye", UnitPrice = 35 , UnitsInStock = 90};
+
+            Product product3 = new Product();
+            product3.Id = 3;
+            product3.CategoryId = 33;
+            product3.ProductName = "Televizyon";
+            product3.UnitPrice = 1000;
+            product3.UnitsInStock = 120;
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Remove(product2);
+            productManager.Update(product3);
+        }
+    }
+}
